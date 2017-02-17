f3f130c56d8315c2e21151eadc2d61f05fab70bf
Ryuukai <manukathu123@gmail.com>
2017-02-17 21:33:55 +0530

    Update config.js

--- config.js
+++ config.js
@@ -2,10 +2,10 @@ var config = {
   token: "MjgyMTY5NjM3NjM3MTkzNzMw.C4imgg.EYQcu-SFFJ1lvCGy0tO8FnuYIxM", //Insert your bot token here, you can get this from https://discordapp.com/developers/applications/me
   twitchClientId: "1v79zvjnxeyocbh5x4i3mnloboyac3", //Because twitch is a dick and requires it now (https://www.twitch.tv/settings/connections)
   ownerID: "110076057167618048", //Your (bot owner's) id for extra access. Can be blank for no owner.
-  cmdPrefix: ".", //The character or string commands must start with. DEFAULT: "!"
+  cmdPrefix: ",", //The character or string commands must start with. DEFAULT: "!"
   allowInvite: true, //Boolean. If true, the !invite command is allowed, which sends an oauth link to add the bot to different servers. DEFAULT: false
 
-  volume: 1.25, //Audio volume. 0 = 0%, 0.5 = 50%, 1 = 100%, etc. Be careful not to set it to 50 (500%) or something similar. DEFAULT: 0.5
+  volume: 1.5, //Audio volume. 0 = 0%, 0.5 = 50%, 1 = 100%, etc. Be careful not to set it to 50 (500%) or something similar. DEFAULT: 0.5
   queueDisplaySize: 10, //The amount of tracks shown with the queue command. DEFAULT: 10
   limitToSummoner: false, //Give only the person who summoned the bot access to !stop or !remove, etc. DEFAULT: true
   voteSkip: false, //Vote for skipping a song, instead of just having one person skipping. DEFAULT: true
